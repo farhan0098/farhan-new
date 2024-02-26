005570e956ef32d4472021730bceb5897db51a2b
Farhan Ali <159815493+farhan0098@users.noreply.github.com>
2024-02-26 23:02:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// var number =200;
+// var number =300;
+// alert()
+// alert("farhan says hello")
+// alert(2024)
+// var greet1 ="khan"
+// alert(greet1)
+// greet1="mumtaz"
+// alert(greet1)
+// greet1="farhan"
+// alert(greet1)
+// var MyQualifacation="matric"
+// alert(MyQualifacation )
+// var number1 =2+2
+// alert(number1)
+var hour=24
+var min=60
+var sec=60
+var
